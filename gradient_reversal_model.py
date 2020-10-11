93805a8e7a1d8f8bd5399ab0c2f96518843c46eb
rubenIng93 <48275113+rubenIng93@users.noreply.github.com>
2020-10-11 19:08:38 +0200

    Update gradient_reversal_model.py

--- gradient_reversal_model.py
+++ gradient_reversal_model.py
@@ -28,17 +28,17 @@ class DANNAlexNet(nn.Module):
         super(DANNAlexNet, self).__init__()
         self.features = nn.Sequential(
             nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
-            nn.ReLu(inplace=True),
+            nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=3, stride=2),
             nn.Conv2d(64, 192, kernel_size=5, padding=2),
-            nn.ReLu(inplace=True),
+            nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=3, stride=2),
             nn.Conv2d(192, 384, kernel_size=3, padding=1),
-            nn.ReLu(inplace=True),
+            nn.ReLU(inplace=True),
             nn.Conv2d(384, 256, kernel_size=3, padding=1),
-            nn.ReLu(inplace=True),
+            nn.ReLU(inplace=True),
             nn.Conv2d(256, 256, kernel_size=3, padding=1),
-            nn.ReLu(inplace=True),
+            nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=3, stride=2),
         )
 
@@ -47,20 +47,20 @@ class DANNAlexNet(nn.Module):
         self.classifier = nn.Sequential(
             nn.Dropout(),
             nn.Linear(256 * 6 * 6, 4096),
-            nn.ReLu(inplace=True),
+            nn.ReLU(inplace=True),
             nn.Dropout(),
             nn.Linear(4096, 4096),
-            nn.ReLu(inplace=True),
+            nn.ReLU(inplace=True),
             nn.Linear(4096, num_classes)
         )
 
         self.dann_classifier = nn.Sequential(
             nn.Dropout(),
             nn.Linear(256 * 6 * 6, 4096),
-            nn.ReLu(inplace=True),
+            nn.ReLU(inplace=True),
             nn.Dropout(),
             nn.Linear(4096, 4096),
-            nn.ReLu(inplace=True),
+            nn.ReLU(inplace=True),
             nn.Linear(4096, 2)
             # it must distinguish between target and source
             # then only 2 classes
